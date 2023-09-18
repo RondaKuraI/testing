d2a73cef2a20ceac1f8fbe30e3608e7472d23243
RondaKuraI <johnronda0527@gmail.com>
2023-09-18 13:39:15 +0800

    database

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -3,9 +3,13 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-
+//use App\Models\ProductModel;
 class ProductController extends BaseController
 {
+    private $product;
+    public function __construct(){
+        $this->product = new \App\Models\ProductModel();
+    }
     public function product($product){
         echo $product;
     }
